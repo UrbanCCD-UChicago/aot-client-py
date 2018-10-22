2558b136b4cb824203e43d7c793e8e120319ce43
Vince Forgione <vforgione@uchicago.edu>
2018-10-22 15:59:46 -0500

    Added Setup

---
+++ setup.py
@@ -0,0 +1,39 @@
+import codecs
+import os
+from setuptools import find_packages, setup
+
+
+# semantic versioning
+VERSION = '0.1.0'
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, 'README.md'), encoding='utf8') as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='aot-client',
+    version=VERSION,
+    description='The Official Python Client of the Array of Things API',
+    long_description=long_description,
+    url='https://github.com/UrbanCCD-UChicago/aot-client-py',
+    author='Vince Forgione',
+    author_email='vforgione@uchicago.edu',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=find_packages(where='.', exclude=['tests', 'docs'])
+)
